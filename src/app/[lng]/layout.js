94a3a2884756da486a9d735bf57fc2f5119eec25
Ebson Joy <ebsonjoy721@gmail.com>
2025-12-08 14:46:59 +0400

    feat: add newsletter subscription feature with toast notifications

--- src/app/[lng]/layout.js
+++ src/app/[lng]/layout.js
@@ -2,6 +2,8 @@ import { I18nextProvider } from '../i18n/client'
 import { fallbackLng, languages } from '../i18n/settings'
 import { useTranslation } from '../i18n/index'
 import ConditionalLayoutHeader from '@/components/layouts/LayoutHeader'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default async function Layout({ children, params }) {
   const { lng } = await params
@@ -13,6 +15,15 @@ export default async function Layout({ children, params }) {
       <ConditionalLayoutHeader lng={lng}>
         {children}
       </ConditionalLayoutHeader>
+      <ToastContainer 
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={lng === 'ar'}
+        pauseOnHover
+        theme="light"
+      />
     </I18nextProvider>
   )
 }
